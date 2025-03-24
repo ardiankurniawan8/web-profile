4dbca7017901f2491fb0970b5438585b35fe41b3
ardiankurniawan8 <ardian.smbc@gmail.com>
2025-03-25 00:51:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import DefaultLayout from './layouts/default.vue'
+</script>
+
+<template>
+  <default-layout>
+    <router-view />
+  </default-layout>
+</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card layout rounded-5">
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-9">
+        <div class="card">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
